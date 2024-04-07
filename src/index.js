4c4854d7f01f9792fec6830bdc10abaf239089ee
frausimon <165206824+frausimon@users.noreply.github.com>
2024-04-07 20:09:05 +0200

    added apiKey + api url

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+function searchCity(city) {
+  let apiKey = "fe2c1d92f67b8973od44d8049abf6b3t";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}units=metric";
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
