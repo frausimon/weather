764d1c3e98a2cd9eb3fcf0307d53b2d1cdab4aeb
frausimon <165206824+frausimon@users.noreply.github.com>
2024-04-14 19:54:48 +0200

    removed forecast html into js, added function display forecast, created string forecast,

--- src/index.js
+++ src/index.js
@@ -56,4 +56,31 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">☀</div>
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max">
+            <strong>15°</strong>
+          </span>
+          <span class="weather-forecast-temperature-min">
+          12°</span>
+        </div>
+     </div>
+   `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 searchCity("Winnipeg");
+displayForecast();
